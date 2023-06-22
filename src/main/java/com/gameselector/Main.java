759661b059a95c23255dbe35525f048348b7f9ee
Aaron Callaway <aaroncallaway31@gmail.com>
2023-06-21 19:25:38 -0500

    variables inserted

--- src/main/java/com/gameselector/Main.java
+++ src/main/java/com/gameselector/Main.java
@@ -1,7 +1,24 @@
 package com.gameselector;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World! & Aaron & Sam!");
     }
+
+    String gameTitle;
+    double rating;
+    double averageWeight;
+    double rank;
+    int minPlayers;
+    int maxPlayers;
+    int playTime;
+    List<Integer> bestPlayerCount = new ArrayList<>();
+    Boolean isStandalone;
+
+
+
 }
